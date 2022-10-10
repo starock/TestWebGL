7463494c231af4034365e820a07c48d2d3241180
stormer <s2010@126.com>
2022-10-10 19:45:57 +0800

    c

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
 module.exports = {
   async headers() {
     return [
       {
-        source: '/pages',
+        source: '/index',
         headers: [
           {
             key: 'Content-Encoding',
